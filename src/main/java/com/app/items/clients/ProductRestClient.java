dfb28df2d8d691a15305d0d4945aae1e9df4e0bb
ellpf <mauriciojavier.lucero.ext@bayer.com>
2020-03-13 00:42:57 -0300

    Eureka client registry

--- src/main/java/com/app/items/clients/ProductRestClient.java
+++ src/main/java/com/app/items/clients/ProductRestClient.java
@@ -1,18 +1,18 @@
 package com.app.items.clients;
 
 import com.app.items.models.Product;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient
+@FeignClient(name = "products-service")
 public interface ProductRestClient {
 
     @GetMapping("/products")
-    public List<Product> getProducts();
+    List<Product> getProducts();
 
     @GetMapping("/products/{id}")
-    public Product getProductById(@PathVariable Long id);
+    Product getProductById(@PathVariable Long id);
 }
